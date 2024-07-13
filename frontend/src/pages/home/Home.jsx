fe2e39bd89dd9c342b60e6ade31f8a639119a59d
masstamilan <arasut075@gmail.com>
2024-07-13 20:51:20 +0530

    test1 response

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,13 +3,16 @@ import MessageContainer from '../../components/messages/MessageContainer';
 
 const Home = () => {
 	return (
-		<div className='h-full'>
-			<h1 className='text-center text-4xl font-bold font-mono mb-10 tracking-widest cursor-pointer'>MassChat.com</h1>
+		<div className='h-full sm:h-auto'>
+			<h1 className='text-center text-4xl font-bold font-mono mb-10 tracking-widest cursor-pointer shadow-md shadow-blue-600 hover:shadow-lg hover:shadow-blue-600 transition-shadow duration-300'>
+				MassChat.com
+			</h1>
 			<div className='flex flex-col sm:flex-row sm:h-[650px] md:h-[550px] md:w-[1000px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 sm:p-0 sm:m-0 sm:w-full'>
-			<Sidebar />
-			<MessageContainer/>
-		</div>
+				<Sidebar />
+				<MessageContainer />
+			</div>
 		</div>
 	);
 };
+
 export default Home;
