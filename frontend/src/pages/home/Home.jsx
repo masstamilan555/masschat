11b86a2bb96623a17834c909719915bb8fbf7ea3
masstamilan <arasut075@gmail.com>
2024-07-13 20:27:38 +0530

    mobile responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,11 +3,12 @@ import MessageContainer from '../../components/messages/MessageContainer';
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[650px] md:h-[550px] md:w-[1000px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 sm:p-0 sm:m-0 sm:w-full'>
+		<div>
+			<h1 className='text-center text-4xl font-bold font-mono mb-10 tracking-widest cursor-pointer'>MassChat.com</h1>
+			<div className='flex flex-col sm:flex-row sm:h-[650px] md:h-[550px] md:w-[1000px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 sm:p-0 sm:m-0 sm:w-full'>
 			<Sidebar />
 			<MessageContainer/>
-
-		
+		</div>
 		</div>
 	);
 };
